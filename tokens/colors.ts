f5a06930824b6e3462db1ab21bddc16bd7a4e1ad
Felipe Gouvea <felipegalvesouvea@gmail.com>
2023-01-31 22:04:16 -0300

    feat: add colors token

---
+++ tokens/colors.ts
@@ -0,0 +1,13 @@
+export const colors = {
+  white: '#fff',
+  black: '#000',
+
+  'gray.100': '#F4F4F4',
+  'gray.200': '#C4C4CC',
+  'gray.400': '#7C7C8A',
+  'gray.500': '#505059',
+  'gray.600': '#323238',
+  'gray.700': '#29292E',
+  'gray.800': '#202024',
+  'gray.900': '#161614',
+}
